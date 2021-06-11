5b12c93661b75b14f229f56516812c9fd82f484d
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2021-06-11 17:24:11 -0500

    Utilziando un segmento para definir la pantalla inicial

--- landing_page_example/lib/providers/page_provider.dart
+++ landing_page_example/lib/providers/page_provider.dart
@@ -7,6 +7,15 @@ class PageProvider extends ChangeNotifier {
   List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
   // int _currentIndex = 0;
 
+  createScrollController(String routeName) {
+    this.scrollController =
+        new PageController(initialPage: getPageIndex(routeName));
+  }
+
+  int getPageIndex(String routeName) {
+    return (_pages.indexOf(routeName) == -1) ? 0 : _pages.indexOf(routeName);
+  }
+
   goTo(int index) {
     final routeName = _pages[index];
     print(routeName);
--- landing_page_example/lib/router/router_handlers.dart
+++ landing_page_example/lib/router/router_handlers.dart
@@ -1,11 +1,17 @@
 import 'package:fluro/fluro.dart';
+import 'package:landing_page_example/providers/page_provider.dart';
 import 'package:landing_page_example/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 final homeHandler = new Handler(
   handlerFunc: (context, params) {
-    print(params);
     final page = params['page']!.first;
 
-    if (page != '/') return HomePage();
+    if (page != '/') {
+      final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+      pageProvider.createScrollController(page);
+
+      return HomePage();
+    }
   },
 );
