6c7f0d10de7d1b420fb1b8b622a7447a63de420f
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2021-06-11 00:40:16 -0500

    Animaciones al menu

--- landing_page_example/lib/ui/shared/custom_app_menu.dart
+++ landing_page_example/lib/ui/shared/custom_app_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'custom_menu_item.dart';
@@ -16,7 +17,6 @@ class _CustomAppMenuState extends State<CustomAppMenu>
   late AnimationController controller;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     controller = AnimationController(
       vsync: this,
@@ -28,54 +28,61 @@ class _CustomAppMenuState extends State<CustomAppMenu>
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        onTap: () {
-          if (isOpen) {
-            controller.reverse();
-          } else {
-            controller.forward();
-          }
+    return FadeInLeft(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () {
+            if (isOpen) {
+              controller.reverse();
+            } else {
+              controller.forward();
+            }
 
-          setState(() {
-            isOpen = !isOpen;
-          });
-        },
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          width: 150,
-          height: isOpen ? 308 : 50,
-          color: Colors.black,
-          child: Column(
-            children: [
-              _MenuTitle(isOpen: isOpen, controller: controller),
-              if (isOpen) ...{
-                CustomMenuItem(
-                  text: 'Home',
-                  onPressed: () {},
-                ),
-                CustomMenuItem(
-                  text: 'About',
-                  onPressed: () {},
-                ),
-                CustomMenuItem(
-                  text: 'Pricing',
-                  onPressed: () {},
-                ),
-                CustomMenuItem(
-                  text: 'Contact',
-                  onPressed: () {},
-                ),
-                CustomMenuItem(
-                  text: 'Location',
-                  onPressed: () {},
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-              }
-            ],
+            setState(() {
+              isOpen = !isOpen;
+            });
+          },
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            width: 150,
+            height: isOpen ? 308 : 50,
+            color: Colors.black,
+            child: Column(
+              children: [
+                _MenuTitle(isOpen: isOpen, controller: controller),
+                if (isOpen) ...{
+                  CustomMenuItem(
+                    text: 'Home',
+                    delay: 0,
+                    onPressed: () {},
+                  ),
+                  CustomMenuItem(
+                    text: 'About',
+                    delay: 30,
+                    onPressed: () {},
+                  ),
+                  CustomMenuItem(
+                    text: 'Pricing',
+                    delay: 60,
+                    onPressed: () {},
+                  ),
+                  CustomMenuItem(
+                    text: 'Contact',
+                    delay: 90,
+                    onPressed: () {},
+                  ),
+                  CustomMenuItem(
+                    text: 'Location',
+                    delay: 120,
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                }
+              ],
+            ),
           ),
         ),
       ),
@@ -103,7 +110,7 @@ class _MenuTitle extends StatelessWidget {
           AnimatedContainer(
             duration: Duration(milliseconds: 200),
             curve: Curves.easeInOut,
-            width: isOpen ? 50 : 0,
+            width: isOpen ? 40 : 0,
           ),
           Text(
             'Menú',
